ad6a70cada7450b8b3fa52bb560ebbf74ba1300f
Mosè Raguzzini <mose.raguzzini@businesscreate.com>
2012-12-13 14:27:23 +0100

    hotfixes

--- jquery.magnify.js
+++ jquery.magnify.js
@@ -13,31 +13,41 @@
 			}).hide().fadeIn();
 			_appendImg = _appendedItem.find('img');
 			_that.bind("mousemove",function(e) {
-				if(_appendedItem.outerHeight() < $(window).height()){
-					//console.log(_appendedItem.outerHeight())
-					if(e.pageY+_appendedItem.outerHeight() <= $(window).scrollTop() + $(window).height() ){
+				var _outerHeight = _appendedItem.outerHeight();
+				var _outerWidth = _appendedItem.outerWidth();
+				var _winHeight = $(window).height();
+				var _winWidth = $(window).width();
+				var _scrollTop = $(window).scrollTop();
+				var _scrollLeft = $(window).scrollLeft();
+				if(_outerHeight < _winHeight){
+					if(e.pageY+_outerHeight <= _scrollTop + _winHeight ){
 						_appendedItem.css('top', e.pageY + _padding);
 					}else{
-						_appendedItem.css('top', e.pageY - _appendedItem.outerHeight() - 10);
+						_appendedItem.css('top', e.pageY - _outerHeight - 10);
 					}
-				}else{
-					_appendedItem.css('top', $(window).scrollTop());
-					_appendImg.css('height', $(window).height()-(_padding*2));
-				}
-				if(_appendedItem.outerWidth() < $(window).width()){
-					if(e.pageX+_appendedItem.outerWidth() <= $(window).scrollLeft() + $(window).width()){
-						_appendedItem.css('left', e.pageX+_padding);
-					}else if(_appendedItem.outerWidth() < e.pageX){
-						_appendedItem.css('left', e.pageX-_appendedItem.outerWidth());
-					}
-					if(_appendedItem.outerWidth() > e.pageX && e.pageX+_appendedItem.outerWidth() > $(window).scrollLeft() + $(window).width()){
-						if(e.pageX >= $(window).width()/2){
-							_appendedItem.css('left', $(window).width()-_appendedItem.outerWidth());
+					if(_outerHeight > e.pageY && e.pageY+_outerHeight > _scrollTop + _winHeight){
+						if(e.pageY >= _winHeight/2){
+							_appendedItem.css('top', _winHeight-_outerHeight);
 						}	
 					}
 				}else{
-					_appendedItem.css('left', $(window).scrollLeft());
-					_appendImg.css('width', $(window).width()-(_padding*2));
+					_appendedItem.css('top', _scrollTop);
+					_appendImg.css('height', _winHeight-(_padding*2));
+				}
+				if(_outerWidth < _winWidth){
+					if(e.pageX+_outerWidth <= _scrollLeft + _winWidth){
+						_appendedItem.css('left', e.pageX + _padding);
+					}else if(_outerWidth < e.pageX){
+						_appendedItem.css('left', e.pageX-_outerWidth);
+					}
+					if(_outerWidth > e.pageX && e.pageX+_outerWidth > _scrollLeft + _winWidth){
+						if(e.pageX >= _winWidth/2){
+							_appendedItem.css('left', _winWidth-_outerWidth);
+						}	
+					}
+				}else{
+					_appendedItem.css('left', _scrollLeft);
+					_appendImg.css('width', _winWidth-(_padding*2));
 				}			
 			})
 		})
